f965dee67918928c97ceab5a978faefbabafa29d
Babita Bhatt <babitabhatta37@gmail.com>
2024-12-13 13:01:28 +0545

    navbar initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
+import Navbar from "./Navbar";
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navbar/>} />
       </Routes>
     </Router>
   );
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="p-4 text-white bg-blue-600">
+      <h1>My Navbar</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
